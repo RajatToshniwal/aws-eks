48b4a2f89c14bc5f3b8fcd6ff0f910c69b894170
RajatToshniwal <rajat.toshniwal@gmail.com>
2021-06-07 11:37:43 +0530

    Copying the kubectl to /usr/loca/bin

--- managerserver_script/installation.sh
+++ managerserver_script/installation.sh
@@ -14,7 +14,8 @@ if [ ! -f $kubectl_file ]; then
 #Kubectl installation
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
 chmod u+x kubectl
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+#mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+cp ./kubectl /usr/local/bin/
 kubectl version
 fi
 eksctl_comm="eksctl"
