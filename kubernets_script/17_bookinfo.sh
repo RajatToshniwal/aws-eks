07ff22c33617479caf409378f302afd73bec49ba
RajatToshniwal <rajat.toshniwal@gmail.com>
2021-06-07 12:18:33 +0530

    Adding the logic for the creation of AWS Application Load balancer

--- kubernets_script/17_bookinfo.sh
+++ kubernets_script/17_bookinfo.sh
@@ -4,3 +4,7 @@ kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml --namespace=bookin
 #######Verifying the installation 
 kubectl get all --namespace=bookinfo
 kubectl exec  "$(kubectl get pod -l  app=ratings --namespace bookinfo -l app=ratings -o jsonpath='{.items[0].metadata.name}')" -c ratings --namespace bookinfo -- curl -sS productpage:9080/productpage | grep -o "<title>.*</title>"
+kubectl apply -f sample_yaml/ingress.yml #This will implement AWS APPLICATION LOAD BALANCER
+kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml #Istio gateway and virtual service implementation
+istioctl analyze
+
